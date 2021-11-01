5778aa91c20a4691b3d16455356f5855b0f96ba8
Kesava Karri <kkeshav1998@gmail.com>
2021-10-31 20:27:12 -0400

    Leetcode 549: Return unique number

---
+++ Arrays/singleNumber.js
@@ -0,0 +1,18 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+*/
+var singleNumber = function(nums) {
+  let single_num = 0;
+  for (let i = 1; i < nums.length + 1; i++) {
+    single_num = single_num ^ nums[i-1];
+  }
+  return single_num;
+};
+
+console.log(singleNumber([4,1,2,1,2]));
