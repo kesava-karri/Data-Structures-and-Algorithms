d9e268b4755c82a820e316375252a33e4a8ec69e
Kesava Karri <kkeshav1998@gmail.com>
2022-01-19 23:35:59 -0500

    Non Constructible Change

---
+++ Arrays/nonConstructibleChange.js
@@ -0,0 +1,11 @@
+function nonConstructibleChange(coins) {
+  coins.sort((a, b) => a - b);
+
+  let currentChange = 0;
+
+  for (let i = 0; i < coins.length; i++) {
+    if (coins[i] > currentChange + 1) return currentChange + 1;
+    currentChange += coins[i];
+  }
+  return currentChange + 1;
+}
