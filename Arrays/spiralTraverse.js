51464a1ba2672764075e431a1b27623d60f14c5d
Kesava Karri <kkeshav1998@gmail.com>
2022-01-28 18:05:00 -0500

    Spiral Traverse

---
+++ Arrays/spiralTraverse.js
@@ -0,0 +1,42 @@
+function spiralTraverse(array) {
+	let result = [];
+	let startRow = 0;
+	let endRow = array.length - 1;
+	let startCol = 0;
+	let endCol = array[0].length - 1;
+	
+	while (startRow <= endRow && startCol <= endCol) {
+		for (let col = startCol; col <= endCol; col++) {
+			result.push(array[startRow][col]);
+		}
+		
+		for (let row = startRow + 1; row <= endRow; row++) {
+			result.push(array[row][endCol]);
+		}
+		
+		for (let col = endCol - 1; col >= startCol; col--) {
+			if (startRow === endRow) break;
+			result.push(array[endRow][col])
+		}
+		
+		for (let row = endRow - 1; row > startRow; row--) {
+			if (startCol === endCol) break;
+			result.push(array[row][startCol]);
+		}
+		
+		startRow++;
+		endRow--;
+		startCol++;
+		endCol--;
+	}
+	return result;
+}
+
+console.log(spiralTraverse(
+	[
+		[1, 2, 3, 4],
+		[12, 13, 14, 5],
+		[11, 16, 15, 6],
+		[10, 9, 8, 7],
+	]
+));
