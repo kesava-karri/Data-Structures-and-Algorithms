4e06ab96e56d6c186576edd0de08f4bfa5e4e297
Keshav K <kkeshav1998@gmail.com>
2023-09-03 00:46:19 -0700

    TwoSumII

--- DSA/Main.java
+++ DSA/Main.java
@@ -1,8 +1,35 @@
 import java.util.Arrays;
 import java.util.HashMap;
 
+class TwoSumIIQ2 {
+    // Note: Given array is sorted in non-decreasing order & it's 1-indexed array
+    // Your solution must use only constant extra space.
+
+    static int[] usingTwoPointers(int[] numbers, int target) {
+        // TC: O(n); SC: O(1)
+        int length = numbers.length;
+        int i = 0;
+        int j = length - 1;
+
+        while (i < j && j > -1) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                break;
+            } else if (sum > target) {
+                j -= 1;
+            } else {
+                i += 1;
+            }
+        }
+        return new int[] { i + 1, j + 1 };
+    }
+
+    // Omitting bruteForce which is nested loops & also uses constant space
+}
+
 class TwoSumQ1 {
     static int[] usingHashMap(int[] nums, int target) {
+        // TC: O(n); SC: O(n)
         HashMap<Integer, Integer> occurenceMap = new HashMap<Integer, Integer>();
 
         for (int num : nums) {
@@ -43,8 +70,8 @@ class TwoSumQ1 {
 
     static int[] usingBinarySearch(int[] nums, int target) {
         // TC: O(nlogn); SC: O(n)
-        // hasn't been properly implemented
-        int low = 0;
+        // hasn't been properly implemented - tech debt
+        int low = 1;
         int high = nums.length - 1;
         int[] temp = Arrays.copyOf(nums, nums.length);
         Arrays.sort(nums);
@@ -52,35 +79,37 @@ class TwoSumQ1 {
         // Assigning random numbers
         int currNum = 0;
         int numToBeSearched = 0;
-        int i = 0;
 
-        forLoop: for (i = 0; i < nums.length; i++) {
+        forLoop: for (int i = 0; i < nums.length; i++) {
             currNum = nums[i];
             numToBeSearched = target - currNum;
-            int mid = (low + high) / 2;
-            int middleNum = nums[mid];
-            while (low < high) {
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                int middleNum = nums[mid];
                 if (middleNum == numToBeSearched) {
                     currNum = nums[i];
                     numToBeSearched = nums[mid];
                     break forLoop;
                 } else if (middleNum > numToBeSearched) {
-                    high = mid;
+                    high = mid - 1;
                 } else {
-                    low = mid;
+                    low = mid + 1;
                 }
             }
         }
 
         // To find the actual index
+        int firstIndex = -1;
         int secondIndex = -1;
+        System.out.println(currNum + "\t" + numToBeSearched);
         for (int k = 0; k < temp.length; k++) {
-            if (numToBeSearched == nums[k] && k != i) {
+            if (currNum == temp[k] && firstIndex == -1) {
+                firstIndex = k;
+            } else if (numToBeSearched == temp[k]) {
                 secondIndex = k;
-                break;
             }
         }
-        return new int[] { i, secondIndex };
+        return new int[] { firstIndex, secondIndex };
     }
 
     static int[] usingTwoPointers(int[] nums, int target) {
@@ -139,12 +168,19 @@ class TwoSumQ1 {
 
 class Main {
     public static void main(String[] args) {
+        // System.out.println(Arrays.toString(TwoSumIIQ2.usingTwoPointers(new int[] { 2, 7, 11, 15 }, 9)));
+        // System.out.println(Arrays.toString(TwoSumIIQ2.usingTwoPointers(new int[] { 2, 3, 4 }, 6)));
+        // System.out.println(Arrays.toString(TwoSumIIQ2.usingTwoPointers(new int[] { -1, 0 }, -1)));
+        // System.out.println(Arrays.toString(TwoSumIIQ2.usingTwoPointers(new int[] { 5, 25, 75 }, 100)));
+
         // System.out.println(Arrays.toString(TwoSumQ1.usingHashMap(new int[] { 2, 7, 11, 15 }, 9)));
         // System.out.println(Arrays.toString(TwoSumQ1.usingHashMap(new int[] { 3, 2, 4 }, 6)));
-        System.out.println(Arrays.toString(TwoSumQ1.usingHashMap(new int[] { 2, 5, 5, 11 }, 6)));
+        // System.out.println(Arrays.toString(TwoSumQ1.usingHashMap(new int[] { 2, 5, 5, 11 }, 6)));
 
         // System.out.println(Arrays.toString(TwoSumQ1.usingBinarySearch(new int[] { 2, 7, 11, 15 }, 9)));
         // System.out.println(Arrays.toString(TwoSumQ1.usingBinarySearch(new int[] { 3, 2, 4 }, 6)));
+        // System.out.println(Arrays.toString(TwoSumQ1.usingBinarySearch(new int[] { 2, 5, 5, 11 }, 6)));
+
         // System.out.println(Arrays.toString(TwoSumQ1.usingBinarySearch(new int[] { 3, 3 }, 6)));
 
         // System.out.println(Arrays.toString(TwoSumQ1.usingTwoPointers(new int[] { 2, 7, 11, 15 }, 9)));
