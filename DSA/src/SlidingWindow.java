8f6c2e0059a1b1522db639e7bcbcdee4cff35735
Keshav K <kkeshav1998@gmail.com>
2023-09-28 18:00:06 -0700

    ContainsDuplicate

--- DSA/src/SlidingWindow.java
+++ DSA/src/SlidingWindow.java
@@ -1,20 +1,172 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 public class SlidingWindow {
-    public static class ContainsDuplicateQ2 {
-        public static boolean solution(int[] nums, int k) {
+    public class SlidingWindowMaximumQ4 {
+        public int[] approach(int[] nums, int k) {
+            List<Integer> ans = new ArrayList();
+            List<Integer> temp = new ArrayList();
+
+            if (nums.length == k) {
+                return new int[] { Arrays.stream(nums).max().getAsInt() };
+            }
+            if (k == 1) {
+                return nums; // since the window is 1, we add all elements resulting in same given array
+            }
+            for (int i = 0; i < k; i++) {
+                temp.add(nums[i]);
+            }
+            int ptr = 0;
+            int tempPtr = 0;
+
+            int temp1 = Collections.max(temp);
+            ans.add(temp1);
+            for (int i = k; i <= nums.length - 1; i++) {
+                if (ans.get(ptr) != temp.get(tempPtr)) {
+                    if (nums[i] > ans.get(ptr)) {
+                        ans.add(nums[i]);
+                        ptr++;
+                    } else {
+                        ans.add(ans.get(ptr));
+                        ptr++;
+                    }
+                    temp.add(nums[i]);
+                    tempPtr++;
+                }
+            }
+
+            return ans.stream().mapToInt(Integer::intValue).toArray();
+        }
+    }
+    public class ContainsDuplicateIIIQ3 {
+        Map<Integer, Integer> shortMap = new HashMap<>();
+        public boolean approach1(int[] nums, int indexDiff, int valueDiff) {
+            System.out.println("1. " + shortMap);
+            int windowSize = indexDiff + 1;
+            int ptr = 0;
+            int firstWindowLen = windowSize > nums.length ? nums.length : windowSize;
+            // first window
+            for (int i = 0; i < firstWindowLen; i++) {
+                shortMap.put(i, nums[i]);
+            }
+            System.out.println("2. " + shortMap);
+
+            return false;
+        }
+
+        public boolean brokenApproach(int[] nums, int indexDiff, int valueDiff) {
+            // 33 out of 50;
+            System.out.println("1. " + shortMap);
+            int windowSize = indexDiff + 1;
+            int ptr = 0;
+            int firstWindowLen = windowSize > nums.length ? nums.length : windowSize;
+            for (int i = 0; i < firstWindowLen; i++) {
+                if (i != shortMap.get(nums[i])) {
+                    if (Math.abs(i - shortMap.get(nums[i])) <= indexDiff
+                            && Math.abs(nums[i] - nums[i]) <= valueDiff) {
+                        return true;
+                    }
+                }
+                shortMap.put(nums[i], i);
+                System.out.println("2. " + shortMap);
+            }
+            shortMap.remove(nums[ptr]);
+            System.out.println("3. " + shortMap);
+            ptr++;
+            for (int i = windowSize; i < nums.length; i++) {
+                int count = windowSize - 1;
+                if (count < windowSize) {
+                    if (shortMap.containsKey(nums[i]) && i != shortMap.get(nums[i])) {
+                        if (Math.abs(i - shortMap.get(nums[i])) <= indexDiff) {
+                            return true;
+                        }
+                    }
+                    shortMap.put(nums[i], i);
+                    System.out.println("4.. " + shortMap);
+                    count++;
+                }
+                shortMap.remove(nums[ptr]);
+                System.out.println("5. " + shortMap);
+                ptr++;
+            }
 
             return false;
         }
     }
 
-    public static class ContainsDuplicateQ1 {
+    public class ContainsDuplicateIIQ2 {
+        Map<Integer, Integer> shortMap = new HashMap<>();
+
+        public boolean approach(int[] nums, int k) {
+            int windowSize = k + 1; // since we need difference of indices, so if k is 3 then windowSize would be 4
+            int ptr = 0;
+            if (nums.length == 1) { // since i, j have to be distinct
+                return false;
+            }
+            // first window
+            int firstWindowLen = windowSize >= nums.length ? nums.length : windowSize;
+            for (int i = 0; i < firstWindowLen; i++) {
+                if (shortMap.containsKey(nums[i]) && i != shortMap.get(nums[i])) {
+                    if (Math.abs(i - shortMap.get(nums[i])) <= k) {
+                        return true;
+                    }
+                }
+                shortMap.put(nums[i], i);
+            }
+            shortMap.remove(nums[ptr]);
+            ptr = ptr + 1;
+
+            for (int j = ptr + 1; j < nums.length; j++) {
+                if (shortMap.containsKey(nums[j]) && j != shortMap.get(nums[j])) {
+                    if (Math.abs(j - shortMap.get(nums[j])) <= k) {
+                        return true;
+                    }
+                }
+                shortMap.put(nums[j], j);
+                if (shortMap.size() == windowSize) {
+                    shortMap.remove(nums[ptr]);
+                    ptr++;
+                }
+            }
+
+            return false;
+        }
+
+        public boolean brokenApproach(int[] nums, int k) {
+            // not storing the values leads to more complex conditions
+
+            if (nums.length - 1 < k) { // since i, j have to be distinct
+                return false;
+            }
+            // first window
+            for (int i = 0; i <= k; i++) {
+                for (int j = i + 1; j <= k; j++) {
+                    if (nums[i] == nums[j] && j - i <= k) {
+                        return true;
+                    }
+                }
+            }
+
+            for (int i = 1; i < nums.length - k; i++) {
+                for (int j = i; j < i + k; j++) {
+                    if (nums[j] == nums[j + 1] && j - i <= k) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public static class ContainsDuplicateIQ1 {
         public static boolean solution(int[] nums) {
             // Input: nums = [1,2,3,1]
             // Output: true
---
+++ DSA/tst/SlidingWindowTest.java
@@ -0,0 +1,112 @@
+package tst;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import org.junit.jupiter.api.Disabled;
+import src.SlidingWindow;
+import src.SlidingWindow.ContainsDuplicateIIIQ3;
+import src.SlidingWindow.ContainsDuplicateIIQ2;
+import src.SlidingWindow.ContainsDuplicateIQ1;
+import src.SlidingWindow.FindAllAnagrams;
+import src.SlidingWindow.LongestSubstringWithoutRepeatingCharacters;
+import src.SlidingWindow.MaxSumOfkConsecutive;
+import src.SlidingWindow.SlidingWindowMaximumQ4;
+import src.SlidingWindow.SubstringsOfSizeThreeWithDistinctCharacters;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SlidingWindowTest {
+    // Please refrain from checking the outputs of a same class together since the classes are static and might result
+    // in unexpected behavior
+    SlidingWindow slidingWindow = new SlidingWindow();
+    /* Constructor Injection */
+    public SlidingWindowTest() {
+    }
+    @Test
+    public void testSlidingWindowMaximumQ4() {
+        SlidingWindowMaximumQ4 o1 = slidingWindow.new SlidingWindowMaximumQ4();
+        assertTrue(Arrays.equals(new int[] {3, 3, 5, 5, 6, 7}, o1.approach(new int[] { 1,3,-1,-3,5,3,6,7 }, 3)));
+
+        SlidingWindowMaximumQ4 o2 = slidingWindow.new SlidingWindowMaximumQ4();
+        assertTrue(Arrays.equals(new int[] { 1,-1 }, o2.approach(new int[] { 1,-1 }, 1)));
+    }
+
+    @Test
+    public void testContainsDuplicateIIIQ3() {
+//        ContainsDuplicateIIIQ3 o3 = slidingWindow.new ContainsDuplicateIIIQ3();
+//        assertTrue(o3.approach1(new int[] {1,0,1,1}, 1, 2));
+/*
+        ContainsDuplicateIIIQ3 o2 = slidingWindow.new ContainsDuplicateIIIQ3();
+        assertTrue(o2.approach1(new int[] {1,2,3,1}, 3, 0));
+*/
+        ContainsDuplicateIIIQ3 o1 = slidingWindow.new ContainsDuplicateIIIQ3();
+        assertFalse(o1.approach1(new int[] {1,5,9,1,5,9}, 2, 3));
+    }
+
+    @Test
+    @Ignore
+    public void testContainsDuplicateIIQ2() {
+        ContainsDuplicateIIQ2 o1 = slidingWindow.new ContainsDuplicateIIQ2();
+        assertTrue(o1.approach(new int[] { 1, 2, 3, 1 }, 3));
+
+        ContainsDuplicateIIQ2 o2 = slidingWindow.new ContainsDuplicateIIQ2();
+        assertTrue(o2.approach(new int[] { 1,0,1,1 }, 1));
+
+//        System.out.println(ContainsDuplicateIIQ2.approach(new int[] { 1,2,3,4,5,6,1 }, 3));
+        ContainsDuplicateIIQ2 o3 = slidingWindow.new ContainsDuplicateIIQ2();
+        assertFalse(o3.approach(new int[] { 1,2,3,4,5,1 }, 3));
+
+        ContainsDuplicateIIQ2 o4 = slidingWindow.new ContainsDuplicateIIQ2();
+//        System.out.println(ContainsDuplicateIIQ2.approach(new int[] { 1,2,3,1,2,3 }, 2));
+        assertFalse(o4.approach(new int[] { 1,2,3,1,2,3 }, 2));
+
+        ContainsDuplicateIIQ2 o5 = slidingWindow.new ContainsDuplicateIIQ2();
+        assertTrue(o5.approach(new int[] { 99, 99 }, 2));
+    }
+
+    @Test
+    public void testContainsDuplicate() {
+        assertTrue(ContainsDuplicateIQ1.solution(new int[] { 1, 2, 3, 1 }));
+        assertFalse(ContainsDuplicateIQ1.solution(new int[] { 1,2,3,4 }));
+        assertTrue(ContainsDuplicateIQ1.solution(new int[] { 1,1,1,3,3,4,3,2,4,2 }));
+    }
+
+    @Test
+    public void testFindAllAnagrams() {
+        /*
+        Having all outputs of FindAllAnagrams might result in unexpected result, since static fields are used which are class variables, so they get modified :)
+         */
+
+//        System.out.println(FindAllAnagrams.approach1("aaa", "aaaa"));
+//        System.out.println(FindAllAnagrams.approach1("cbaebabacd", "abc"));
+        System.out.println(FindAllAnagrams.approach1("abab", "ab"));
+//        System.out.println(FindAllAnagrams.approach1("baa", "aa"));
+    }
+
+    @Test
+    public void testLongestSubstringWithoutRepeatingCharacters() {
+        System.out.println(LongestSubstringWithoutRepeatingCharacters.solution("abcabcbb"));
+        /*
+        System.out.println(LongestSubstringWithoutRepeatingCharacters.solution(" "));
+        System.out.println(LongestSubstringWithoutRepeatingCharacters.solution("au"));
+        System.out.println(LongestSubstringWithoutRepeatingCharacters.solution("aab"));
+         */
+    }
+
+    @Test
+    public void testSubstringsOfSizeThreeWithDistinctCharacters() {
+//        System.out.println(SubstringsOfSizeThreeWithDistinctCharacters.solution("xyzzaz"));
+        System.out.println(SubstringsOfSizeThreeWithDistinctCharacters.solution("aababcabc"));
+    }
+
+    @Test
+    public void testThisMaxSumOfkConsecutive() {
+        int result = MaxSumOfkConsecutive.solution(new int[] { 100, 200, 300, 400 }, 2);
+        assertEquals(700, result);
+    }
+}
