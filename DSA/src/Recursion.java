00061b167034a992623189edfe64e8e8828a4491
Keshav K <kkeshav1998@gmail.com>
2023-10-24 03:21:28 -0700

    ValidPalindrome, Factorial

--- DSA/src/Recursion.java
+++ DSA/src/Recursion.java
@@ -1,34 +1,89 @@
 package src;
 
-class ReverseString {
-    public static void approach1(char[] s) {
-        recursion(s, 0, s.length - 1);
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class Recursion {
+    public class Factorial {
+        public int solution(int n) {
+            return factorial(n);
+        }
+
+        public int factorial(int n) {
+            if (n <= 1) return 1;
+            return n * factorial(n - 1);
+        }
     }
 
-    private static void recursion(char[] s, int i, int j) {
-        if (i >= j) return;
-        char temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-        recursion(s, i+1, j-1);
-    }
-}
+    public class ValidPalindrome {
+        public boolean isPalindrome(String s) {
+            // Removing non-alphanumeric values
+            s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+            int i = 0, j = s.length() - 1;
+            return recursion(s, i, j);
+        }
 
-class PowerOfTwo {
-    public boolean isPowerOfTwo(int n) {
-        return divideByTwo(n);
+        public boolean recursion(String str, int i, int j) {
+            if (i >= j) return true;
+            return str.charAt(i) == str.charAt(j) && recursion(str, i + 1, j - 1);
+        }
     }
 
-    private boolean divideByTwo(int i) {
-        if (i == 0) return false;
-        if (i == 1) return true;
-        return i % 2 == 0 && divideByTwo(i / 2);
-    }
-}
+    /**
+     public class Palindrome {
+     private final String str;
 
-class Recursion {
-    public static void main (String[] args) {
-//        ReverseString.approach1(new char[] {'h','e','l','l','o'});
-//        System.out.println(PowerOfTwo.isPowerOfTwo(16));
+     public Palindrome(String str) {
+     this.str = str;
+     }
+
+     public boolean solution(int i , int j) {
+     if (i >= j) return true;
+     return str.charAt(i) == str.charAt(j) && solution(i + 1, j - 1);
+     }
+
+     public boolean checkIfPalindrome(String str, int i, int j) {
+     return false;
+     }
+     }*/
+
+    public class ReverseAString {
+        public String solution(String str) {
+            int i = str.length() - 1;
+            String ans = "";
+            return reverseString(str, ans, i);
+        }
+
+        public String reverseString(String str, String ans, int i) {
+            if (i < 0) return ans;
+            String tempStr = String.valueOf(str.charAt(i));
+            return reverseString(str, ans.concat(tempStr), --i);
+        }
+    }
+
+    class ReverseString {
+        public void approach1(char[] s) {
+            recursion(s, 0, s.length - 1);
+        }
+
+        private void recursion(char[] s, int i, int j) {
+            if (i >= j) return;
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            recursion(s, i+1, j-1);
+        }
+    }
+
+    class PowerOfTwo {
+        public boolean isPowerOfTwo(int n) {
+            return divideByTwo(n);
+        }
+
+        private boolean divideByTwo(int i) {
+            if (i == 0) return false;
+            if (i == 1) return true;
+            return i % 2 == 0 && divideByTwo(i / 2);
+        }
     }
 }
---
+++ DSA/tst/RecursionTest.java
@@ -0,0 +1,63 @@
+package tst;
+
+import org.junit.Test;
+import src.Recursion;
+import src.Recursion.CountGoodNumbers;
+import src.Recursion.Factorial;
+import src.Recursion.ReverseAString;
+import src.Recursion.ValidPalindrome;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RecursionTest {
+    Recursion recursionObj = new Recursion();
+
+    @Test
+    public void testCountGoodNumbers() {
+        CountGoodNumbers o1 = recursionObj.new CountGoodNumbers();
+        System.out.println(o1.countGoodNumbers(50));
+    }
+    @Test
+    public void testReverseAString() {
+        String str = "Keshav";
+        ReverseAString o1 = recursionObj.new ReverseAString();
+        assertEquals("vahseK", o1.solution(str));
+    }
+
+    @Test
+    public void testPalindrome() {
+        String oddLengthStr = "ABCABACBA";
+        String oddLengthStrNonP = "ABC";
+        String evenLengthStr = "ABCAACBA";
+        String evenLengthStrNonP = "race a car";
+        String s = "A man, a plan, a canal: Panama";
+        String singleSpace = " ";
+
+
+
+        ValidPalindrome o1 = recursionObj.new ValidPalindrome();
+        assertTrue(o1.isPalindrome(s));
+        assertTrue(o1.isPalindrome(singleSpace));
+
+        assertTrue(o1.isPalindrome(oddLengthStr));
+        assertTrue(o1.isPalindrome(evenLengthStr));
+        assertFalse(o1.isPalindrome(oddLengthStrNonP));
+        assertFalse(o1.isPalindrome(evenLengthStrNonP));
+    }
+    /**@Test
+    public void testPalindrome() {
+        String str = "ABCABACBA";
+        Palindrome o1 = recursionObj.new Palindrome(str);
+        int i = 0, j = str.length() - 1;
+        assertTrue(o1.solution(i, j));
+    }*/
+
+    @Test
+    public void testFactorial() {
+        Factorial o1 = recursionObj.new Factorial();
+        assertEquals(120, o1.solution(5));
+        assertEquals(720, o1.solution(6));
+    }
+}
