b03aa6b2607243d80d7f0c025ebc3c6401630daa
Keshav K <kkeshav1998@gmail.com>
2023-12-05 01:58:55 -0800

    Minimum Depth of Binary Tree

--- DSA/src/BinaryTreeAndBinarySearchTree.java
+++ DSA/src/BinaryTreeAndBinarySearchTree.java
@@ -51,4 +51,26 @@ public class BinaryTreeAndBinarySearchTree {
             f(root.right);
         }
     }
+
+    class MinDepthOfBT {
+        public int minDepth(TreeNode root) {
+            if (root == null) return 0;
+            return f(root);
+        }
+
+        private int f(TreeNode root) {
+            // For this problem preorder traversal is optimal approach
+            if (root.left == null && root.right == null) return 1;
+            int leftDepth = Integer.MAX_VALUE, rightDepth = Integer.MAX_VALUE;
+            if (root.left != null) {
+                leftDepth = f(root.left);
+            }
+            if (root.right != null) {
+                rightDepth = f(root.right);
+            }
+            return Math.min(leftDepth, rightDepth) + 1;
+        }
+    }
+
+
 }
