da89f107ac129bc3ba17bbd2be2abec9f85aebce
Keshav K <kkeshav1998@gmail.com>
2024-09-16 16:27:13 -0700

    2619. Array Prototype Last

---
+++ CodeChronicles/14. Array Prototype Last/Array Prototype Last.ts
@@ -0,0 +1,32 @@
+interface Array<T> {
+  last(): T | -1;
+}
+
+Array.prototype.last = function () {
+  const lastIndex = this.length - 1;
+  return lastIndex < 0 ? -1 : this[lastIndex];
+};
+
+Array.prototype.last = function () {
+  const lastIndex = this.length - 1;
+  return lastIndex < 0 ? -1 : this.at(lastIndex);
+};
+
+Object.defineProperty(Array.prototype, "last", {
+  value: function () {
+    const lastIndex = this.length - 1;
+    return lastIndex < 0 ? -1 : this[lastIndex];
+  },
+});
+
+Object.assign(Array.prototype, {
+  last() {
+    const lastIndex = this.length - 1;
+    return lastIndex < 0 ? -1 : this[lastIndex];
+  },
+});
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
