6abfad3975e33e596e210f4e7ca10f3a4f722599
Keshav K <kkeshav1998@gmail.com>
2024-09-22 15:58:33 -0700

    2704. To Be Or Not To Be| --updated flag method

--- CodeChronicles/18. To Be Or Not To Be/To Be Or Not To Be.ts
+++ CodeChronicles/18. To Be Or Not To Be/To Be Or Not To Be.ts
@@ -10,34 +10,35 @@ enum Throw {
 
 class expectUsingClass {
   constructor(private val: any) {}
-  toBe(num: any): boolean {
-    return this.val === num || _throw(Throw.NotEqual);
+  toBe(ele: any): boolean {
+    return this.val === ele || _throw(Throw.NotEqual);
   }
-  notToBe(num: any): boolean {
-    return this.val !== num || _throw(Throw.Equal);
+  notToBe(ele: any): boolean {
+    return this.val !== ele || _throw(Throw.Equal);
   }
 }
 const expect = (val: any) => new expectUsingClass(val);
 
 // ------------------------------------------------
 function expectUsingFlag(val: any): ToBeOrNotToBe {
-  let flag: boolean = false;
-  const toBe = function (num: any): boolean {
-    return val === num ? (flag = true) : _throw("Not Equal");
-  };
-  const notToBe = function (num: any): boolean {
-    return val !== num ? (flag = true) : _throw("Equal");
+  const toBe = (ele: any): boolean => callThis(ele, false);
+  const notToBe = (ele: any): boolean => callThis(ele, true);
+  const callThis = (ele: any, flag: boolean): boolean => {
+    if (flag === (val === ele)) {
+      flag ? _throw(Throw.Equal) : _throw(Throw.NotEqual);
+    }
+    return true;
   };
   return { toBe, notToBe };
 }
 
 // ------------------------------------------------
 function _expect(val: any): ToBeOrNotToBe {
-  const toBe = function (num: any): boolean {
-    return val === num || _throw("Not Equal");
+  const toBe = function (ele: any): boolean {
+    return val === ele || _throw("Not Equal");
   };
-  const notToBe = function (num: any): boolean {
-    return val !== num || _throw("Equal");
+  const notToBe = function (ele: any): boolean {
+    return val !== ele || _throw("Equal");
   };
   return { toBe, notToBe };
 }
