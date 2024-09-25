3d35cfc62e7e30bd59009515a38b1095e4c8c104
Keshav K <kkeshav1998@gmail.com>
2024-09-25 16:51:26 -0700

    2648. Generate Fibonacci Sequence --update: Optimized solution & used destructuring assignment

--- CodeChronicles/19. Generate Fibonacci Sequence/Generate Fibonacci Sequence.ts
+++ CodeChronicles/19. Generate Fibonacci Sequence/Generate Fibonacci Sequence.ts
@@ -6,43 +6,34 @@ function fibIteratorProtocol(): Iterator<number> {
 class Fib implements Iterator<number> {
   private first = 0;
   private last = 1;
-  private res = 0;
   next = () => {
     const obj = {
       done: false,
       value: this.first,
     };
-    this.res = this.first + this.last;
-    this.first = this.last;
-    this.last = this.res;
+    [this.first, this.last] = [this.last, this.first + this.last];
     return obj;
   };
 }
 
 // -----------------------------------------------------------
 function* fibGenerator(): Generator<number, any, number> {
-  yield 0;
-  yield 1;
   yield* f(0, 1);
 }
 
 function* f(first: number, last: number): Generator<number, any, number> {
-  let res = first + last;
-  yield res;
-  yield* f(last, res);
+  yield first;
+  first = first + last;
+  last = first - last;
+  yield* f(first, last);
 }
 // -----------------------------------------------------------
 function* _fibGenerator(): Generator<number, any, number> {
   let first = 0,
-    last = 1,
-    res = 0;
-  yield first;
-  yield last;
+    last = 1;
   while (true) {
-    res = first + last;
-    yield res;
-    first = last;
-    last = res;
+    yield first;
+    [first, last] = [last, first + last];
   }
 }
 
