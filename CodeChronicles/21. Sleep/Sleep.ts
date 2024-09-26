3f1a0ecee0691e05d437e5c13cbbe77edcd7f3c4
Keshav K <kkeshav1998@gmail.com>
2024-09-25 22:11:00 -0700

    2621. Sleep

---
+++ CodeChronicles/21. Sleep/Sleep.ts
@@ -0,0 +1,17 @@
+function sleepUsingSetInterval(millis: number): Promise<void> {
+  return new Promise((resolve) => {
+    const intervalID = setInterval(() => {
+      resolve();
+      clearInterval(intervalID);
+    }, millis);
+  });
+}
+
+function sleep(millis: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
+
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
